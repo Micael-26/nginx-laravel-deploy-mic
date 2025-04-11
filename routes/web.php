bdf7c4e335a4cb1ab47df660846552586908db5b
micael <dinymicael998@gmail.com>
2025-04-11 12:33:59 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('portfolio');
 });
 
 Route::get('/health', function () {
