4a63bf440557b39e7fdd79b62d0021f6111d6009
micael <dinymicael998@gmail.com>
2025-04-13 09:22:47 +0200

    Update configs

--- config/database.php
+++ config/database.php
@@ -82,15 +82,28 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
+        'pgsql' => env('APP_ENV') === 'production' ? [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'sslmode' => env('DB_SSLMODE', 'require'), // 'require' recommandé sur Render
+        ] : [
+            'driver' => 'pgsql',
+            'url' => env('DB_URL'),
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'laravel_db'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', '@ambanja203!'),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
         ],
-    
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DB_URL'),
