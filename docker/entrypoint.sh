ac9f08a6b203c68af6e87b690896f80bcd5cec0f
micael <dinymicael998@gmail.com>
2025-04-14 15:28:05 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,10 @@
 # Exécuter les migrations (elles échoueront silencieusement si PostgreSQL n'est pas prêt)
 php artisan migrate --force || true
 
-# Cache
+# Discover packages (nécessaire si on a utilisé --no-scripts dans le Dockerfile)
+php artisan package:discover --ansi
+
+# Cache Laravel
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
